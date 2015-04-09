c160c7128bf1fc5dbe8ffd38c7af239ce8181969
Stanislaw Masalskyi <admiral.happiness@gmail.com>
2015-04-09 20:22:37 +0300

    Extractng replies

---
+++ resources/views/main/replies.blade.php
@@ -0,0 +1,22 @@
+<article id="replies" class="container">
+    <div class="inner">
+        <div class="reply">
+            <div class="image pull-left"><img src="/images/avatar1.jpg" alt=""/></div>
+            <div class="name">Жанна Левина</div>
+            <div class="content">Нью-Йорк - самый многонациональный и самый лингвистический город мира. Его жители
+                говорят больше чем на 800 языках. Самые "неанглоговорящие" в Нью-Йорке жители Квинса...
+            </div>
+            <a href="#" class="more">{{trans('texts.read_more')}}</a>
+        </div>
+        <div class="reply">
+            <div class="image pull-left"><img src="/images/avatar1.jpg" alt=""/></div>
+            <div class="name">Жанна Левина</div>
+            <div class="content">Нью-Йорк - самый многонациональный и самый лингвистический город мира. Его жители
+                говорят больше чем на 800 языках. Самые "неанглоговорящие" в Нью-Йорке жители Квинса...
+            </div>
+            <a href="#" class="more">{{trans('texts.read_more')}}</a>
+        </div>
+        <br/>
+        <a class="more-replies" href="#">{{trans('texts.more_replies')}}</a>
+    </div>
+</article>

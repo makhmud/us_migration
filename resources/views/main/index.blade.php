1eb4dfa9c4a795a5ed1a6bbf7e8885aa49e1072c
Stanislaw Masalskyi <admiral.happiness@gmail.com>
2015-04-14 19:39:52 +0300

    Front images

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -7,7 +7,7 @@
         <div class="paragraph">
             <div class="show pull-left" >
                 <img src="/images/city1.jpg" alt=""/>
-                <a rel="lightbox[gallery]" href="/images/city1.jpg">{{trans('texts.view')}}</a>
+                <a rel="lightbox[gallery]" href="/images/front/1.jpg">{{trans('texts.view')}}</a>
             </div>
             <h1>{{$part1->title}}</h1>
           {!!$part1->content!!}
@@ -16,7 +16,7 @@
         <div class="paragraph">
             <div class="show pull-right" >
                 <img src="/images/city1.jpg" alt=""/>
-                <a rel="lightbox[gallery]" href="/images/city1.jpg">{{trans('texts.view')}}</a>
+                <a rel="lightbox[gallery]" href="/images/front/2.jpg">{{trans('texts.view')}}</a>
             </div>
             {!!$part2->content!!}
         </div>
@@ -24,7 +24,7 @@
         <div class="paragraph">
             <div class="show pull-left" >
                 <img src="/images/city1.jpg" alt=""/>
-                <a rel="lightbox[gallery]" href="/images/city1.jpg">{{trans('texts.view')}}</a>
+                <a rel="lightbox[gallery]" href="/images/front/3.jpg">{{trans('texts.view')}}</a>
             </div>
             {!!$part3->content!!}
         </div>
